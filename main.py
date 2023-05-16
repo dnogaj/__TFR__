5584e811a8bf626e6e7e31fc21e80d4dbfe5a310
Hubert <121463460+norka02@users.noreply.github.com>
2023-05-16 20:04:41 +0200

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from menu.main_menu import MainMenu
-
-if __name__ == "__main__":
-    """Starting program"""
-    print("Hello in TFR -> best game on the Mars planet")
-    menu_start = MainMenu()
-    menu_start.menus()
